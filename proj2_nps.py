ef050c438e237d0671e80ad1f678f5935b4bfce8
Carlo Sugatan <carlosugatan@gmail.com>
2018-11-14 22:24:22 -0500

    added __str__() method

--- proj2_nps.py
+++ proj2_nps.py
@@ -7,13 +7,6 @@ from alternate_advanced_caching import Cache
 import requests
 from datetime import datetime
 
-## you can, and should add to and modify this class any way you see fit
-## you can add attributes and modify the __init__ parameters,
-##   as long as tests still pass
-##
-## the starter code is here just to make the tests run (and fail)
-
-
 ##############
 # SETTING UP #
 ##############
@@ -54,7 +47,7 @@ def process(response):
         url_lst.append("https://www.nps.gov" + url.h3.a.get('href')+"index.htm")
     # print(url_lst)
 
-    ## Not sure if I'm caching this
+    # Not sure if I'm caching this
     for urls in url_lst:
         response2 = requests.get(urls)
         if response2 == None:
@@ -77,20 +70,34 @@ def process(response):
 
         ## Address ZIP
         address_zip = soup2.find(attrs={"itemprop": "postalCode"})
-        print(address_zip.string)
+        # print(address_zip.string)
+
+
+## you can, and should add to and modify this class any way you see fit
+## you can add attributes and modify the __init__ parameters,
+##   as long as tests still pass
+##
+## the starter code is here just to make the tests run (and fail)
 
 class NationalSite():
-    def __init__(self, type, name, desc, url=None):
+    # def __init__(self, type, name, desc, url=None):
+    def __init__(self, type, name):
         self.type = type
         self.name = name
-        self.description = desc
-        self.url = url
+        # self.description = desc
+        # self.url = url
 
         # needs to be changed, obvi.
         self.address_street = '123 Main St.'
         self.address_city = 'Smallville'
         self.address_state = 'KS'
         self.address_zip = '11111'
+
+    def __str__(self):
+        return "{} ({}): {} {} {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state,self.address_zip)
+
+# michigan = NationalSite('National Lakeshore', 'Sleeping Bear Dunes')
+
 
 
 ## you can, and should add to and modify this class any way you see fit
