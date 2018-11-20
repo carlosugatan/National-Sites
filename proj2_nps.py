b1099faa42ac4960e77567d83664c18df6127985
Carlo Sugatan <carlosugatan@gmail.com>
2018-11-20 13:13:24 -0500

    Fixed creating new NationalSites instances

--- proj2_nps.py
+++ proj2_nps.py
@@ -19,6 +19,7 @@ def create_id(site, topic):
 def process(response):
     name_lst = []
     url_lst = []
+    site_lst = []
 
     ## use the `response` to create a BeautifulSoup object
     soup = BeautifulSoup(response, 'html.parser')
@@ -31,60 +32,59 @@ def process(response):
         name = container.h3.text
         name_lst.append(name)
         # print(name)
-    #
+
     #     # Type
-    #     type = container.h2.text
+        type = container.h2.text
     #     # print(type)
-    #
+
     #     # Description
-    #     process.desc = container.p.text
+        process.desc = container.p.text
     #     # print(desc)
-    #
+
     #     # URL
-    #     process.url = "https://www.nps.gov"+container.h3.a.get('href')+"index.htm"
-    #     url_lst.append(process.url)
+        process.url = "https://www.nps.gov"+container.h3.a.get('href')+"index.htm"
+        url_lst.append(process.url)
     #     # print(url)
-    #
-    #
-    #     for urls in url_lst:
-    #         cache_file = "nps_address.json"
-    #         site="nps.gov"
-    #         topic="National Sites Address"
-    #         cache_address = Cache(cache_file)
-    #
-    #         UID = create_id(site, topic)
-    #         response2 = cache_address.get(UID)
-    #         if response2 == None:
-    #             response2 = requests.get(urls).text
-    #             cache_address.set(UID, response2, 1)
-    #         # get_address(response2)
-    #
-    #         soup2 = BeautifulSoup(response2, "html.parser")
-    #
-    #         # ## Address Street
-    #         address_street_fndr = soup2.find(attrs={"itemprop": "streetAddress"})
-    #         process.address_street = address_street_fndr.text
-    #         # print(process.address_street)
-    #
-    #         ## Address City
-    #         address_city_fndr = soup2.find(attrs={"itemprop": "addressLocality"})
-    #         process.address_city = address_city_fndr.text
-    #         # print(process.address_city)
-    #
-    #         # ## Address State
-    #         address_state_fndr = soup2.find(attrs={"itemprop": "addressRegion"})
-    #         process.address_state = address_state_fndr.text
-    #         # print(process.address_state)
-    #
-    #         # ## Address ZIP
-    #         address_zip_fndr = soup2.find(attrs={"itemprop": "postalCode"})
-    #         process.address_zip = address_zip_fndr.text
-    #         # print(process.address_zip)
-    #
-    #         national_sites = NationalSite(type, name)
-    #     # print(national_sites)
-    #             # name_lst.append(name)
-    # # return name_lst
+
+        # for urls in url_lst:
+        #     cache_file = "nps_address.json"
+        #     site="nps.gov"
+        #     topic="National Sites Address"
+        #     cache_address = Cache(cache_file)
+        #
+        #     UID = create_id(site, topic)
+        #     response2 = cache_address.get(UID)
+        #     if response2 == None:
+        #         response2 = requests.get(urls).text
+        #         cache_address.set(UID, response2, 1)
+        #     # get_address(response2)
+        #
+        #     soup2 = BeautifulSoup(response2, "html.parser")
+        #
+        #     # ## Address Street
+        #     address_street_fndr = soup2.find(attrs={"itemprop": "streetAddress"})
+        #     process.address_street = address_street_fndr.text
+        #     # print(process.address_street)
+        #
+        #     ## Address City
+        #     address_city_fndr = soup2.find(attrs={"itemprop": "addressLocality"})
+        #     process.address_city = address_city_fndr.text
+        #     # print(process.address_city)
+        #
+        #     # ## Address State
+        #     address_state_fndr = soup2.find(attrs={"itemprop": "addressRegion"})
+        #     process.address_state = address_state_fndr.text
+        #     # print(process.address_state)
+        #
+        #     # ## Address ZIP
+        #     address_zip_fndr = soup2.find(attrs={"itemprop": "postalCode"})
+        #     process.address_zip = address_zip_fndr.text
+        #     # print(process.address_zip)
+
+        national_sites = NationalSite(type, name)
+        site_lst.append(national_sites)
+    # print(national_sites.description)
+    return site_lst
 
 
 ######################
@@ -114,10 +114,17 @@ def google_coordinates(input, inputtype="textquery", fields="formatted_address,g
     params_diction["fields"] = fields
     params_diction["key"] = google_places_key
     unique_rep = params_unique_combination(baseurl, params_diction,private_keys=["key"])
-    print(unique_rep)
+    # print(unique_rep)
     data = c2.get(unique_rep)
     if data:
-        print("Data in cache")
+        with open ("google_coordinates.json", 'r') as f:
+            key_dict = unique_rep.upper()
+            coordinates = json.load(f)
+            lng = str(coordinates[key_dict]["values"]["candidates"][0]["geometry"]["location"]["lng"])
+            lat = str(coordinates[key_dict]["values"]["candidates"][0]["geometry"]["location"]["lat"])
+            google_coordinates.location = lat+","+lng
+            # print(google_coordinates.location)
+        # print("Data in cache")
         return data
     else:
         resp = requests.get(baseurl, params=params_diction)
@@ -133,10 +140,14 @@ def google_nearby_places(location, radius=10000):
     params_diction["radius"] = radius
     params_diction["key"] = google_places_key
     unique_rep = params_unique_combination(baseurl, params_diction,private_keys=["key"])
-    print(unique_rep)
+    # print(unique_rep)
     data = c.get(unique_rep)
     if data:
-        print("Data in cache")
+        # print("Data in cache")
+        with open ("google_places.json", 'r') as f:
+            key_dict = unique_rep.upper()
+            nearbyplaces = json.load(f)
+            # print(nearbyplaces[key_dict]["values"]["results"])
         return data
     else:
         resp = requests.get(baseurl, params=params_diction)
@@ -144,10 +155,14 @@ def google_nearby_places(location, radius=10000):
         c.set(unique_rep, obj, 10)
         return obj
 
-
+# restaurant = google_coordinates("Sleeping Bear Dunes National Lakeshore")
+# restaurant = google_nearby_places(google_coordinates.location)
 # restaurant = google_nearby_places("-33.8599358,151.2090295")
-restaurant = google_coordinates("Museum of Contemporary Art Australia")
 
+
+######################
+#   NATIONAL SITES   #
+######################
 
 class NationalSite():
     def __init__(self, type, name):
@@ -156,17 +171,19 @@ class NationalSite():
         self.name = name
         self.description = process.desc
         self.url = process.url
-
-        self.address_street = process.address_street
-        self.address_city = process.address_city
-        self.address_state = process.address_state
-        self.address_zip = process.address_zip
+        #
+        # self.address_street = process.address_street
+        # self.address_city = process.address_city
+        # self.address_state = process.address_state
+        # self.address_zip = process.address_zip
 
     def __str__(self):
-        return "{} ({}): {} {}, {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state,self.address_zip)
+        # return "{} ({}): {} {}, {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state,self.address_zip)
         # return "{} ({}) {}".format(self.name, self.type, self.address_city)
-        # return "{} ({})".format(self.name, self.type)
+        return "{} ({})".format(self.name, self.type)
 
+    def __repr__(self):
+        return "{} {}".format(self.name, self.type)
 
 class NearbyPlace():
     def __init__(self, name):
@@ -182,18 +199,20 @@ class NearbyPlace():
 ##        (e.g., National Parks, National Heritage Sites, etc.) that are listed
 ##        for the state at nps.gov
 def get_sites_for_state(state_abbr):
-    return process(response)
+    return process(response) # returns NationalSites instances of that state
     # print(name_lst)
     # return process(response)
     # return []
 
 
-## Must return the list of NearbyPlaces for the specifite NationalSite
+## Must return the list of NearbyPlaces for the specific NationalSite
 ## param: a NationalSite object
 ## returns: a list of NearbyPlaces within 10km of the given site
 ##          if the site is not found by a Google Places search, this should
 ##          return an empty list
-def get_nearby_places_for_site(national_site):
+def get_nearby_places(national_site):
+    example_site = google_coordinates(national_site)
+    example_site2 = google_nearby_places(google_coordinates.location)
     return []
 
 ## Must plot all of the NationalSites listed for the state on nps.gov
@@ -226,7 +245,6 @@ cache = Cache(cache_file)
 base_org = "https://www.nps.gov/state/%%/index.htm"
 base = base_org.replace('%%', state_abbr)
 
-
 ######################
 #    RUN PROGRAM     #
 ######################
@@ -238,5 +256,45 @@ if response == None:
 process(response)
 
 
+## TESTING GET SITES FOR STATE
 # xy = get_sites_for_state(state_abbr)
 # print(xy)
+
+
+## TESTING NATIONAL SITES CLASS
+# national_sites = NationalSite("National Lakeshore", "Sleeping Bear Dunes")
+# print(national_sites.url)
+
+
+## TESTING GET NEARBY PLACES
+# z = get_nearby_places(NationalSite("National Lakeshore", "Sleeping Bear Dunes"))
+# print(z)
+
+
+
+
+## THIS IS JUNK BUT I'LL KEEP IT HERE
+# with open ("nps.json", 'r') as f2:
+#     nps_json = json.load(f2)
+#     soup = BeautifulSoup(response, 'html.parser')
+#     national_site_container = soup.find_all('div', class_ = 'col-md-9 col-sm-9 col-xs-12 table-cell list_left')
+#
+#     for container in national_site_container:
+#
+#         # Name
+#         name = container.h3.text
+#         print(name)
+
+# with open ("nps_address.json", 'r') as f3:
+#     nps_address_json = json.load(f3)
+#
+#     soup2 = BeautifulSoup(response2, "html.parser")
+#     ## Address Street
+#     address_street_fndr = soup2.find(attrs={"itemprop": "streetAddress"})
+#     address_street = address_street_fndr.text
+#     print(address_street)
+
+    # print(nps_json)
+    # lng = str(coordinates[key_dict]["values"]["candidates"][0]["geometry"]["location"]["lng"])
+    # lat = str(coordinates[key_dict]["values"]["candidates"][0]["geometry"]["location"]["lat"])
+    # google_coordinates.location = lng+","+lat
