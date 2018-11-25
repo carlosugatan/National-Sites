6f6fa6e44b115dfeeb296e4c2eb63cbcfaad753f
Carlo Sugatan <carlosugatan@gmail.com>
2018-11-25 14:50:43 -0500

    updated secrets.py with Google API and Plotly API

--- secrets.py
+++ secrets.py
@@ -1,2 +1,6 @@
+import plotly
 # put your google api key here
-google_places_key = ''
+google_places_key = 'AIzaSyDIOyj7CIiitsEtNIQA4FcYhnHX7QNnMzE'
+
+# plotly
+plotly.tools.set_credentials_file(username='explodingcars', api_key='PoewHNwF0bx1zuE8lP6i')
